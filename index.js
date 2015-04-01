cfea2038e7d84a8a70a0b0da0ffebcc86fbb4895
Shaun Cutts <shaunc@factfiber.com>
2015-04-01 19:54:14 -0400

    fixed anachronistic index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // require coffee if possible; js otherwise
 try {
   require('coffee-script/register');  
-  AsyncProxyPool = require('./src/index');
+  AsyncProxyPool = require('./src/async-proxy-pool');
 }
 catch (e) {
   if(e.message.indexOf("Cannot find module") != -1 
       && (e.message.indexOf('./src/index') != -1 
         || e.message.indexOf('coffee-script/register') != -1))
-    AsyncProxyPool = require('./lib/index');
+    AsyncProxyPool = require('./lib/async-proxy-pool');
   else
     throw e;
 }
