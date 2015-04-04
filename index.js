107e0e534c896a32dbb78b4a14c3101447e03ca1
Shaun Cutts <shaunc@factfiber.com>
2015-04-04 13:04:08 -0400

    fixes reference in index when coffee isn't available

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // require coffee if possible; js otherwise
 try {
-  require('coffee-script/register');  
+  try { require('coffee-script/register'); } catch (e) {}  
   AsyncProxyPool = require('./src/async-proxy-pool');
 }
 catch (e) {
